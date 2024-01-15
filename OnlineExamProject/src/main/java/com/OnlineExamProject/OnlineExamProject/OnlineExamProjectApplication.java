f488024502ac9b12eb11d655a59ec21e0e34f0ea
UjwalaMali <156416125+UjwalaMali@users.noreply.github.com>
2024-01-15 18:40:42 +0530

    Project Part 1

---
+++ OnlineExamProject/src/main/java/com/OnlineExamProject/OnlineExamProject/OnlineExamProjectApplication.java
@@ -0,0 +1,18 @@
+package com.OnlineExamProject.OnlineExamProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com")
+@EntityScan("com")
+public class OnlineExamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamProjectApplication.class, args);
+		System.out.println("ok...");
+	}
+
+}
